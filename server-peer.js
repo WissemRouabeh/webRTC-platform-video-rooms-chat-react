45fdb6669bb26772c32605bdcf237a068234fcf6
WissemRouabeh <wissem19962009@gmail.com>
2021-02-26 03:34:57 +0100

    azesd

--- server-peer.js
+++ server-peer.js
@@ -8,38 +8,38 @@ const io = socket(server);
 const users = {};
 const PORT = process.env.PORT || 8003;
 
-// io.on("connection", (socket) => {
-//   socket.on("joinroom", (payload) => {
-//     console.log(payload);
+io.on("connection", (socket) => {
+  socket.on("joinroom", (payload) => {
+    console.log(payload);
 
-//     socket.join(payload.roomID);
+    socket.join(payload.roomID);
 
-//     socket
-//       .to(payload.roomID)
-//       .broadcast.emit("userconnected", { id: payload.id, name: payload.sname });
-//     socket.on("disconnect", () => {
-//       socket.to(payload.roomID).broadcast.emit("userdisconnected", payload.id);
-//     });
+    socket
+      .to(payload.roomID)
+      .broadcast.emit("userconnected", { id: payload.id, name: payload.sname });
+    socket.on("disconnect", () => {
+      socket.to(payload.roomID).broadcast.emit("userdisconnected", payload.id);
+    });
 
-//     // socket
-//     //   .to(payload.roomID)
-//     //   .broadcast.emit("calling", (payload.id, payload.sname));
-//   });
+    // socket
+    //   .to(payload.roomID)
+    //   .broadcast.emit("calling", (payload.id, payload.sname));
+  });
 
-//   socket.on("joinmsg", (roomid) => {
-//     socket.join(roomid);
-//     console.log("ahla fi " + roomid + " where ur id " + socket.id);
-//     socket.on("sendingmsg", (msg) => {
-//       console.log(msg);
-//       socket.to(roomid).broadcast.emit("receivemsg", msg);
-//     });
-//   });
+  socket.on("joinmsg", (roomid) => {
+    socket.join(roomid);
+    console.log("ahla fi " + roomid + " where ur id " + socket.id);
+    socket.on("sendingmsg", (msg) => {
+      console.log(msg);
+      socket.to(roomid).broadcast.emit("receivemsg", msg);
+    });
+  });
 
-//   socket.on("joinothers", (payload) => {
-//     socket.join(payload.roomid);
-//     socket.to(payload.roomid).broadcast.emit("newuser", payload.id);
-//   });
-// });
+  socket.on("joinothers", (payload) => {
+    socket.join(payload.roomid);
+    socket.to(payload.roomid).broadcast.emit("newuser", payload.id);
+  });
+});
 const rout = require("./route");
 app.use("/", rout);
 
@@ -47,4 +47,4 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("p2peer/build"));
 }
 
-server.listen(8003, () => console.log("server is running on port " + PORT));
+server.listen(PORT, () => console.log("server is running on port " + PORT));
